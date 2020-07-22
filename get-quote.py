6832987f7bc7a157ee76e533fad38b77a60b1af0
unknown <dimitris_46@hotmail.com>
2020-07-22 20:31:41 +0300

    2 rnd quotes bot

--- get-quote.py
+++ get-quote.py
@@ -6,8 +6,10 @@ def primary():
   quotes = f.readlines()
   f.close()
   last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd = random.randint(0, 6)
+  rnd2 = random.randint(7, last)
+  print(quotes[rnd],quotes[rnd2])
+  #print(quotes[rnd2])
 
 if __name__== "__main__":
   primary()
